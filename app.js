2eec4be05af8a61e875ed4c1a4801e3d5b66d2fb
Kossinus-sudo <liberalkoss202@gmail.com>
2025-05-22 10:28:03 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const firebaseConfig = {
+apiKey: "AIzaSyDBLxpmz1pCvix2l0mW-jAgMDea3E5muAI",
+  authDomain: "paynreceive-b7ee9.firebaseapp.com",
+  projectId: "paynreceive-b7ee9",
+  storageBucket: "paynreceive-b7ee9.firebasestorage.app",
+  messagingSenderId: "931007110605",
+  appId: "1:931007110605:web:45b2a88445954acc263884"
+  };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+function checkStatus() {
+  const code = document.getElementById("trackingCode").value.trim();
+  const resultDiv = document.getElementById("result");
+
+  if (!code) {
+    resultDiv.innerText = "Veuillez entrer un code valide.";
+    return;
+  }
+
+  db.collection("colis").doc(code).get()
+    .then(doc => {
+      if (doc.exists) {
+        const data = doc.data();
+        resultDiv.innerText = `Statut : ${data.statut} | Destination : ${data.destination}`;
+      } else {
+        resultDiv.innerText = "Code introuvable. Vérifiez votre code.";
+      }
+    })
+    .catch(error => {
+      resultDiv.innerText = "Erreur de connexion.";
+      console.error("Erreur :", error);
+    });
+}
